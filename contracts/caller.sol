1b29649a6ce642ffad238390d3cd4d4ec02a77c2
g3459 <49g3459@protonmail.com>
2024-12-20 12:16:45 +0100

    callersol

--- contracts/caller.sol
+++ contracts/caller.sol
@@ -66,55 +66,57 @@ contract CCaller {
                 if (poolCall & PID_MASK == UNIV2_PID) {
                     bytes4 tokenSel = direc ? IUniswapV2Pair.token0.selector : IUniswapV2Pair.token1.selector;
                     assembly {
-                        mstore(0x80, tokenSel)
-                        pop(staticcall(gas(), pool, 0x80, 0x04, 0x80, 0x20))
-                        let token := mload(0x80)
-                        mstore(0x80, TRANSFER_SEL)
-                        mstore(0x84, pool)
-                        mstore(0xa4, amIn)
-                        pop(call(gas(), token, 0, 0x80, 0x44, 0, 0))
+                        mstore(0, tokenSel)
+                        pop(staticcall(gas(), pool, 0, 0x04, 0, 0x20))
+                        let token := mload(0)
+                        mstore(0, TRANSFER_SEL)
+                        mstore(0x04, pool)
+                        mstore(0x24, amIn)
+                        pop(call(gas(), token, 0, 0, 0x44, 0, 0))
                     }
                     bytes4 swapSel = IUniswapV2Pair.swap.selector;
                     assembly {
-                        mstore(0x80, swapSel)
-                        mstore(0xc4, address())
-                        mstore(0xe4, 0x80)
-                        mstore(0x104, 0)
+                        mstore(0, swapSel)
+                        mstore(0x44, address())
+                        mstore(0x64, 0x80)
+                        mstore(0x84, 0)
                     }
                     amIn = uint256(uint48(poolCall >> 168)) << uint8(poolCall >> 160);
                     (uint256 amOut0, uint256 amOut1) = direc ? (uint256(0), amIn) : (amIn, uint256(0));
                     assembly {
-                        mstore(0x84, amOut0)
-                        mstore(0xa4, amOut1)
-                        if iszero(call(gas(), pool, 0, 0x80, 0xa4, 0, 0)) {
+                        mstore(0x04, amOut0)
+                        mstore(0x24, amOut1)
+                        if iszero(call(gas(), pool, 0, 0, 0xa4, 0, 0)) {
                             revert(0, 0)
                         }
                     }
                 } else {
                     bytes4 swapSel = IUniswapV3Pool(pool).swap.selector;
                     assembly {
-                        mstore(0x80, swapSel)
-                        mstore(0x84, address())
-                        mstore(0xa4, direc)
-                        mstore(0xc4, amIn)
+                        mstore(0, swapSel)
+                        mstore(0x04, address())
+                        mstore(0x24, direc)
+                        mstore(0x44, amIn)
                     }
                     uint256 sqrtL = direc ? 4295128740 : 1461446703485210103287273052203988822378723970341;
                     assembly {
-                        mstore(0xe4, sqrtL)
-                        mstore(0x104, 0xa0)
-                        mstore(0x124, 0)
-                        if iszero(call(gas(), pool, 0, 0x80, 0xc4, 0x80, 0x40)) {
+                        mstore(0x64, sqrtL)
+                        mstore(0x84, 0xa0)
+                        if iszero(call(gas(), pool, 0, 0, 0xc4, 0, 0x40)) {
                             revert(0, 0)
                         }
                     }
-                    int256 am0;
-                    int256 am1;
-                    assembly {
-                        am0 := mload(0x80)
-                        am1 := mload(0xa0)
+                    int256 amDel;
+                    if (direc) {
+                        assembly {
+                            amDel := mload(0x20)
+                        }
+                    } else {
+                        assembly {
+                            amDel := mload(0x00)
+                        }
                     }
-                    amIn = uint256(-(am0 < am1 ? am0 : am1));
-                    require(amIn >= uint256(uint48(poolCall >> 168)) << uint8(poolCall >> 160));
+                    require((amIn = uint256(-amDel)) >= uint256(uint48(poolCall >> 168)) << uint8(poolCall >> 160));
                 }
             }
         }
