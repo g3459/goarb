341c2ca6887c471def7b99add91f26d76109e9ad
g3459 <49g3459@protonmail.com>
2025-01-28 10:53:33 +0100

    asasdsad

--- contracts/router.sol
+++ contracts/router.sol
@@ -135,7 +135,7 @@ contract CRouter {
 
     function protGas(uint256 pid) internal pure returns (uint256) {
         unchecked {
-            return pid == ALGB_PID ? 300000 : 120000;
+            return pid == ALGB_PID ? 300000 : 150000;
         }
     }
 
